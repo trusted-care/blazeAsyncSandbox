7da98131af273a416bea8022382b9f0414b71297
Daniel Dornhardt <daniel@dornhardt.com>
2023-09-26 12:14:17 +0300

    Some progress generating await withComputation pyramid!

--- babelPlugin/plugin.js
+++ babelPlugin/plugin.js
@@ -2,6 +2,16 @@ let numVisitedCounter = 0;
 let numVisitedClient = 0;
 let numSkippedBecauseServerCounter = 0;
 
+const generate = require("@babel/generator").default;
+
+function truncate(str, length = 100) {
+    return str.length > length ? str.slice(0, length) + "..." : str;
+}
+
+function log(title = '', path) {
+    console.log(title, generate(path.node).code)
+}
+
 module.exports = function ({ types: t, template, caller }) {
     let callerInfo;
     caller(function (c) {
@@ -16,15 +26,28 @@ module.exports = function ({ types: t, template, caller }) {
         let body = path.node.body.body;
         for (let i = 0; i < body.length; i++) {
             let stmt = body[i];
+            let containsAwait = false;
+
             if (t.isExpressionStatement(stmt) && t.isAwaitExpression(stmt.expression)) {
+                containsAwait = true;
+            } else if (t.isVariableDeclaration(stmt)) {
+                for (let declar of stmt.declarations) {
+                    if (t.isAwaitExpression(declar.init)) {
+                        containsAwait = true;
+                        break;
+                    }
+                }
+            }
+
+            if (containsAwait) {
                 if (i < body.length - 1) {
                     const afterAwait = body.splice(i + 1);
 
                     const wrapper = template(`
-                        return Tracker.withComputation(____secretCurrentComputation____, () => {
-                            BODY;
-                        });
-                    `)({ BODY: afterAwait });
+                    return Tracker.withComputation(____secretCurrentComputation____, async () => {
+                        BODY;
+                    });
+                `)({ BODY: afterAwait });
 
                     body.splice(i + 1, 0, wrapper);
                     i += afterAwait.length; // Skip the inserted block
@@ -51,6 +74,7 @@ module.exports = function ({ types: t, template, caller }) {
                     `, { preserveComments: true });
 
                     path.get('body').unshiftContainer('body', initCode);
+
                     wrapFollowingStatementsInBlock(path);
                 }
             },
