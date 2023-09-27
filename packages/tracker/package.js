530267ddcf16f92b858ccbed2ed8e6f96a748ef7
Daniel Dornhardt <daniel@dornhardt.com>
2023-09-27 12:51:15 +0300

    Adding Meteor 2.13 (I think) Tracker package

---
+++ packages/tracker/package.js
@@ -0,0 +1,19 @@
+Package.describe({
+  summary: "Dependency tracker to allow reactive callbacks",
+  version: '1.3.2',
+});
+
+Package.onUse(function (api) {
+  api.use("ecmascript");
+  api.addFiles("tracker.js");
+  api.export("Tracker");
+  api.export("Deps");
+  api.addAssets("tracker.d.ts", ["client", "server"]);
+});
+
+Package.onTest(function (api) {
+  api.use('tinytest');
+  api.use('test-helpers');
+  api.use('tracker');
+  api.addFiles('tracker_tests.js', 'client');
+});
